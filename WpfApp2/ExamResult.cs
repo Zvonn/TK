d1d9dc5ecad058d117f6a0ee341e6dac40e57712
227102 <227102@KB37-120-C11>
2025-05-23 09:20:52 +0300

    добавлены summary

--- WpfApp2/ExamResult.cs
+++ WpfApp2/ExamResult.cs
@@ -15,11 +15,25 @@ namespace WpfApp2
 
     public interface IExamCalculatorService
     {
+        /// <summary>
+        /// Вычисляет результаты экзамена на основе полученных баллов за задания
+        /// </summary>
+        /// <param name="task1">Баллы за задание 1 в строковом формате</param>
+        /// <param name="task2">Баллы за задание 2 в строковом формате</param>
+        /// <param name="task3">Баллы за задание 3 в строковом формате</param>
+        /// <param name="task4">Баллы за задание 4 в строковом формате</param>
+        /// <returns>Объект ExamResult с результатами проверки, суммой баллов и итоговой оценкой</returns>
         ExamResult CalculateResults(string task1, string task2, string task3, string task4);
     }
 
     public class ExamCalculatorService : IExamCalculatorService
     {
+        /// <summary>
+        /// Основной метод расчета экзаменационных результатов
+        /// Проверяет валидность входных данных для каждого задания,
+        /// вычисляет общую сумму баллов и определяет итоговую оценку.
+        /// При обнаружении ошибок валидации добавляет их в результат.
+        /// </summary>
         public ExamResult CalculateResults(string task1, string task2, string task3, string task4)
         {
             var result = new ExamResult();
@@ -37,6 +51,13 @@ namespace WpfApp2
             return result;
         }
 
+        /// <summary>
+        /// Валидирует баллы за задание
+        /// </summary>
+        /// <param name="input">Входное значение баллов</param>
+        /// <param name="maxScore">Максимально допустимый балл для задания</param>
+        /// <param name="taskName">Название задания для сообщений об ошибках</param>
+        /// <param name="result">Объект результатов для записи ошибок</param>
         private void ValidateTask(string input, int maxScore, string taskName, ExamResult result)
         {
             if (!int.TryParse(input, out int score))
@@ -51,6 +72,16 @@ namespace WpfApp2
             }
         }
 
+        /// <summary>
+        /// Определяет итоговую оценку по пятибалльной шкале на основе суммы баллов
+        /// </summary>
+        /// <param name="totalPoints">Общая сумма набранных баллов</param>
+        /// <returns>
+        /// 5 - при 90+ баллов,
+        /// 4 - при 70-89 баллов,
+        /// 3 - при 50-69 баллов,
+        /// 2 - при менее 50 баллов
+        /// </returns>
         private int CalculateGrade(int totalPoints)
         {
             if (totalPoints >= 90) return 5;
